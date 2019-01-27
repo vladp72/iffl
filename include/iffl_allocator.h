3d8140be10d65978661f967a78a1d210c4811388
Vladimir Petter <vladp72@hotmail.com>
2019-01-26 21:49:17 -0800

    split header to common, allocator and collection

---
+++ include/iffl_allocator.h
@@ -0,0 +1,146 @@
+#pragma once
+//
+// Author: Vladimir Petter
+// github: https://github.com/vladp72/iffl
+//
+//
+// This module implements
+// debug_memory_resource a memory resource that can be used along
+// with polimorfic allocator for debugging contained.
+//
+//
+
+#include <iffl_common.h>
+
+//
+// intrusive flat forward list
+//
+namespace iffl {
+
+//
+// This class can be used with Polimorfic Memory Allocator.
+// Forward Linked List has typedef for PMR called pmr_flat_forward_list.
+//
+// Sample usage:
+// iffl::debug_memory_resource dbg_memory_resource;
+// iffl::pmr_flat_forward_list<FLAT_FORWARD_LIST_TEST> ffl{ &dbg_memory_resource };
+// Code should guarantee that memory resource is destroyed after all containers that 
+// use it are destroyed.
+//
+// This calss provides some basic buffer overrun and underrun detection, and
+// also diring destruction checks that we did not leak any allocated blocks.
+//
+//   |---------------------|-------------------|----------------------------------|------------|----------------------------------|
+//   sizeof(size_t) bytes  pointer to          sizeof(size_t) prefix               user data       sizeof(size_t) suffix
+//   offset to the end of  memory resource     0xBEEFABCD - allocated block        filled with     0xDEADABCD - allocated block
+//   allocation                                0xBEEFABCE - deallocated block      0xFE            0xDEADABCE - deallocated block
+//
+// On deallocation it checks that prefix and suffix contain expected pattern
+// On allocation user buffer is filled with 0xFE to help detecting use of uninitialized memory
+//
+class debug_memory_resource 
+    : public std::pmr::memory_resource {
+
+public:
+
+    debug_memory_resource() = default;
+
+    ~debug_memory_resource() {
+        validate_no_busy_blocks();
+    }
+
+    size_t get_busy_blocks_count() const {
+        return busy_blocks_count_.load(std::memory_order_relaxed);
+    }
+
+    void validate_no_busy_blocks() const {
+        FFL_CODDING_ERROR_IF(0 < get_busy_blocks_count());
+    }
+
+private:
+
+    using pattern_type = size_t;   
+
+    struct prefix_t {
+        size_t size;
+        size_t alignment;
+        debug_memory_resource *memory_resource;
+        pattern_type pattern;
+    };
+
+    struct suffix_t {
+        pattern_type pattern;
+    };
+
+    constexpr static size_t min_allocation_size{ sizeof(prefix_t) + sizeof(suffix_t) };
+
+    constexpr static pattern_type busy_block_prefix_pattern{ 0xBEEFABCD };
+    constexpr static pattern_type free_block_prefix_pattern{ 0xBEEFABCE };
+    constexpr static pattern_type busy_block_suffix_pattern{ 0xDEADABCD };
+    constexpr static pattern_type free_block_suffix_pattern{ 0xDEADABCE };
+
+    constexpr static int fill_pattern{ 0xFE };
+
+    void* do_allocate(size_t bytes, size_t alignment) override {
+
+        size_t bytes_with_debug_info = bytes + min_allocation_size;
+        void *ptr = _aligned_malloc(bytes_with_debug_info, alignment);
+        if (nullptr == ptr) {
+            throw std::bad_alloc{};
+        }
+
+        prefix_t *prefix{ reinterpret_cast<prefix_t *>(ptr) };
+        suffix_t *suffix{ reinterpret_cast<suffix_t *>( reinterpret_cast<char *>(ptr) + bytes_with_debug_info) - 1 };
+
+        prefix->size = bytes_with_debug_info;
+        prefix->alignment = alignment;
+        prefix->memory_resource = this;
+        prefix->pattern = busy_block_prefix_pattern;
+        suffix->pattern = busy_block_suffix_pattern;
+
+        increment_busy_block_count();
+
+        char *user_buffer = reinterpret_cast<char *>(prefix + 1);
+        fill_buffer(user_buffer, fill_pattern, bytes);
+
+        return user_buffer;
+    }
+
+    void do_deallocate(void* p, size_t bytes, size_t alignment) noexcept override {
+
+        decrement_busy_block_count();
+
+        prefix_t *prefix{ reinterpret_cast<prefix_t *>(p) - 1 };
+        FFL_CODDING_ERROR_IF_NOT(prefix->pattern == busy_block_prefix_pattern);
+        FFL_CODDING_ERROR_IF_NOT(prefix->memory_resource == this);
+        FFL_CODDING_ERROR_IF_NOT(prefix->size > min_allocation_size);
+        size_t user_allocation_size = prefix->size - min_allocation_size;
+        FFL_CODDING_ERROR_IF_NOT(user_allocation_size == bytes);
+        FFL_CODDING_ERROR_IF_NOT(prefix->alignment == alignment);
+        suffix_t *suffix{ reinterpret_cast<suffix_t *>(reinterpret_cast<char *>(prefix) + prefix->size) - 1 };
+        FFL_CODDING_ERROR_IF_NOT(suffix->pattern == busy_block_suffix_pattern);
+        prefix->pattern = free_block_prefix_pattern;
+        suffix->pattern = free_block_suffix_pattern;
+
+        _aligned_free(prefix);
+    }
+
+    bool do_is_equal(memory_resource const & other) const noexcept override {
+        return (&other == static_cast<memory_resource const *>(this));
+    }
+
+    void increment_busy_block_count() {
+        size_t prev_busy_blocks_count = busy_blocks_count_.fetch_add(1, std::memory_order_relaxed);
+        FFL_CODDING_ERROR_IF_NOT(prev_busy_blocks_count >= 0);
+    }
+
+    void decrement_busy_block_count() {
+        size_t prev_busy_blocks_count = busy_blocks_count_.fetch_sub(1, std::memory_order_relaxed);
+        FFL_CODDING_ERROR_IF_NOT(prev_busy_blocks_count > 0);
+    }
+
+    //bool enable_detecting
+    std::atomic<size_t> busy_blocks_count_{ 0 };
+};
+
+} // namespace iffl
---
+++ include/iffl_common.h
@@ -0,0 +1,224 @@
+#pragma once
+//
+// Author: Vladimir Petter
+// github: https://github.com/vladp72/iffl
+//
+// Common definitions and utility functions.
+//
+#include <type_traits>
+#include <memory>
+#include <memory_resource>
+#include <vector>
+#include <tuple>
+#include <algorithm>
+#include <utility>
+#include <atomic>
+
+#include <cerrno>
+
+#include <intrin.h>
+
+//
+// Assertions
+//
+#ifndef FFL_CRASH_APPLICATION
+#define FFL_CRASH_APPLICATION() {__debugbreak();}
+#endif
+
+#ifndef FFL_CODDING_ERROR_IF
+#define FFL_CODDING_ERROR_IF(C) if (C) {FFL_CRASH_APPLICATION();} else {;}
+#endif
+
+#ifndef FFL_CODDING_ERROR_IF_NOT
+#define FFL_CODDING_ERROR_IF_NOT(C) if (C) {;} else {FFL_CRASH_APPLICATION();}
+#endif
+
+//
+// Field offset in the structure
+//
+#define FFL_FIELD_OFFSET(T, F) (((char const *)(&((T *)NULL)->F)) - (char const *)NULL)
+//
+// Field offset in the structure plus field size. 
+// Offset to the start of padding [if any] for the next field [if any].
+//
+#define FFL_SIZE_THROUGH_FIELD(T, F) (FFL_FIELD_OFFSET(T, F) + sizeof(((T *)NULL)->F))
+//
+// Offset to the start of padding [if any] for the next field [if any].
+//
+#define FFL_PADDING_OFFSET_AFTER_FIELD(T, F) FFL_SIZE_THROUGH_FIELD(T, F)
+//
+// Size of padding [if any] between two fields.
+// Does not validate that F2 is following after F1 in the structure.
+//
+#define FFL_PADDING_BETWEEN_FIELDS_UNSAFE(T, F1, F2)(FFL_FIELD_OFFSET(T, F2) - FFL_SIZE_THROUGH_FIELD(T, F1))
+//
+// Size of padding [if any] between two fields.
+// Validate that F2 is following after F1 in the structure.
+//
+#define FFL_PADDING_BETWEEN_FIELDS(T, F1, F2)([] {static_assert(FFL_FIELD_OFFSET(T, F1) <= FFL_FIELD_OFFSET(T, F2), "F1 must have lower offset in structure than F2"); }, FFL_PADDING_BETWEEN_FIELDS_UNSAFE(T, F1, F2))
+//
+// Calculates pointer to the beginnig of the structure 
+// from the pointer to a field in that structure.
+//
+#define FFL_FIELD_PTR_TO_OBJ_PTR(T, F, P) ((T *)((char const *)(P) - FFL_FIELD_OFFSET(T, F)))
+
+//
+// intrusive flat forward list
+//
+namespace iffl {
+
+//
+// If min or max are defined as a macro then 
+// save current definition, and undefine it
+// at the end of the header we will restore 
+// saved definition
+//
+#ifdef min 
+#define FFL_SAVED_MIN_DEFINITION min
+#undef min
+#endif
+
+#ifdef max 
+#define FFL_SAVED_MAX_DEFINITION max
+#undef max
+#endif
+
+inline static size_t const iffl_npos = std::numeric_limits<size_t>::max();
+
+//
+// restore saved min and max definition
+//
+#ifdef FFL_SAVED_MAX_DEFINITION
+#define max FFL_SAVED_MAX_DEFINITION
+#undef FFL_SAVED_MAX_DEFINITION
+#endif
+
+#ifdef FFL_SAVED_MIN_DEFINITION
+#define min FFL_SAVED_MIN_DEFINITION
+#undef FFL_SAVED_MIN_DEFINITION
+#endif
+
+template<typename T>
+constexpr inline size_t roundup_size_to_alignment(size_t size) noexcept {
+    static_assert(alignof(T) > 0, "make sure algorithm does not break");
+    return ((size + alignof(T)-1) / alignof(T)) *alignof(T);
+}
+
+template<typename T>
+constexpr inline char * roundup_ptr_to_alignment(char *ptr) noexcept {
+    static_assert(alignof(T) > 0, "make sure algorithm does not break");
+    return ((ptr + alignof(T)-1) / alignof(T)) *alignof(T);
+}
+
+template<typename T>
+constexpr inline char const * roundup_ptr_to_alignment(char const *ptr) noexcept {
+    static_assert(alignof(T) > 0, "make sure algorithm does not break");
+    return ((ptr + alignof(T)-1) / alignof(T)) *alignof(T);
+}
+
+inline void copy_data(char *to_buffer, char const *from_buffer, size_t length) noexcept {
+    memcpy(to_buffer, from_buffer, length);
+}
+
+inline void move_data(char *to_buffer, char const *from_buffer, size_t length) noexcept {
+    memmove(to_buffer, from_buffer, length);
+}
+
+inline void fill_buffer(char *buffer, int value, size_t length) noexcept {
+    memset(buffer, value, length);
+}
+
+inline void zero_buffer(char *buffer, size_t length) noexcept {
+    fill_buffer(buffer, 0, length);
+}
+
+//
+// flat_forward_list uses this
+// helper to deallocate memory
+// on failures
+//
+template <typename G>
+class scope_guard :public G {
+
+public:
+
+    constexpr scope_guard(G g)
+        : G{ g } {
+    }
+
+    constexpr scope_guard(scope_guard &&) = default;
+    constexpr scope_guard &operator=(scope_guard &&) = default;
+
+    constexpr scope_guard(scope_guard &) = delete;
+    constexpr scope_guard &operator=(scope_guard &) = delete;
+
+    ~scope_guard() noexcept {
+        discharge();
+    }
+
+    void discharge() noexcept {
+        if (armed_) {
+            this->G::operator()();
+            armed_ = false;
+        }
+    }
+
+    constexpr void disarm() noexcept {
+        armed_ = false;
+    }
+
+    constexpr void arm() noexcept {
+        armed_ = true;
+    }
+
+    constexpr bool is_armed() const noexcept {
+        return armed_;
+    }
+
+    explicit operator bool() const noexcept {
+        return is_armed();
+    }
+
+private:
+    bool armed_{ true };
+};
+
+//
+// Creates an instance of guard given
+// input functor. Helper for lambdas where 
+// we do not know name of lambda type
+//
+template <typename G>
+inline constexpr auto make_scope_guard(G &&g) {
+    return scope_guard<G>{std::forward<G>(g)};
+}
+
+template <typename T>
+struct container_element_type_base {
+    using value_type = T;
+    using pointer = T * ;
+    using const_pointer = T const *;
+    using reference = T & ;
+    using const_reference = T const &;
+    using size_type = std::size_t;
+    using difference_type = std::ptrdiff_t;
+};
+
+//
+// Helper class used as a parameter in 
+// flat_forward_list constructor to designate
+// that it should take ownership of the
+// buffer rather than make a copy.
+//
+struct attach_buffer {};
+
+//
+// Helper class used in flat_forward_list::detach
+// designating that output should contain address 
+// of last element and end of buffer rahter than 
+// theirs offsets
+//
+struct as_pointers {};
+
+} // namespace iffl
+
