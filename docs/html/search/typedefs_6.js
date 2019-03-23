a406cfca26313c1b0ca65942302ef3095d62409b
Vladimir Petter <vladp72@hotmail.com>
2019-03-22 23:29:37 -0700

    Pushing updated documentation

--- docs/html/search/typedefs_6.js
+++ docs/html/search/typedefs_6.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['is_5fdetected',['is_detected',['../namespaceiffl_1_1mpl.html#aa469046f9dd3f5ecb17eb523fce34ca6',1,'iffl::mpl']]],
-  ['is_5fdetected_5fconvertable',['is_detected_convertable',['../namespaceiffl_1_1mpl.html#adf6fdb06a0e47fbcd712561e636cbed1',1,'iffl::mpl']]],
+  ['is_5fdetected_5fconvertible',['is_detected_convertible',['../namespaceiffl_1_1mpl.html#a68f02cacfc9b171000c5816da6391fdc',1,'iffl::mpl']]],
   ['is_5fdetected_5fexact',['is_detected_exact',['../namespaceiffl_1_1mpl.html#a2c6c6c6aa3cbf1eb146e9f197a846d7e',1,'iffl::mpl']]],
   ['is_5fdetected_5ft',['is_detected_t',['../namespaceiffl_1_1mpl.html#a6db054a1babac2c9eeb9d7fc6b13fad2',1,'iffl::mpl']]],
   ['iterator',['iterator',['../classiffl_1_1flat__forward__list__ref.html#a67aa09c32ee4a1db2529f0c4e9864ddb',1,'iffl::flat_forward_list_ref::iterator()'],['../classiffl_1_1flat__forward__list.html#a59bf6a6f46962a1fbaf0d2f7d91098f2',1,'iffl::flat_forward_list::iterator()']]],
--- docs/html/search/variables_6.js
+++ docs/html/search/variables_6.js
@@ -4,7 +4,7 @@ var searchData=
   ['is_5fconst',['is_const',['../structiffl_1_1buffer__t.html#ae94a4ba99d593c46b2c8290d1a120bfb',1,'iffl::buffer_t']]],
   ['is_5fconst_5fiterator',['is_const_iterator',['../classiffl_1_1flat__forward__list__iterator__t.html#a4ac376108e54bd094bb7ea3b57305ac4',1,'iffl::flat_forward_list_iterator_t']]],
   ['is_5fconst_5fiterator_5fv',['is_const_iterator_v',['../classiffl_1_1flat__forward__list__iterator__t.html#aa89752067ce94d001a2e1c51b6829a02',1,'iffl::flat_forward_list_iterator_t']]],
-  ['is_5fdetected_5fconvertable_5fv',['is_detected_convertable_v',['../namespaceiffl_1_1mpl.html#a664a678993dd32a60941f8922a635475',1,'iffl::mpl']]],
+  ['is_5fdetected_5fconvertible_5fv',['is_detected_convertible_v',['../namespaceiffl_1_1mpl.html#ab8e1ec5663d220344903dd9e60a7ab6c',1,'iffl::mpl']]],
   ['is_5fdetected_5fexact_5fv',['is_detected_exact_v',['../namespaceiffl_1_1mpl.html#a8cb199eddf79c01cfb3bb3023a418d6a',1,'iffl::mpl']]],
   ['is_5fdetected_5fv',['is_detected_v',['../namespaceiffl_1_1mpl.html#afac0434045e5cf64ebb4d731266ffc56',1,'iffl::mpl']]],
   ['is_5fnon_5fconst_5fiterator_5fv',['is_non_const_iterator_v',['../classiffl_1_1flat__forward__list__iterator__t.html#a59acdc5720d1d999d280feb639ce68f4',1,'iffl::flat_forward_list_iterator_t']]],
